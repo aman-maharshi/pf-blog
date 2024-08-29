87ca2ab61752ff8175fa503b343e69a886743928
aman-maharshi <amanm4harshi@gmail.com>
2024-08-29 13:04:57 +0530

    next themes init

---
+++ components/providers.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from 'react'
+import { ThemeProvider } from 'next-themes'
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      enableSystem={true}
+      attribute='class'
+      defaultTheme='system'
+      disableTransitionOnChange={true}
+    >
+      {children}
+    </ThemeProvider>
+
+  )
+}
+
+export default Providers
