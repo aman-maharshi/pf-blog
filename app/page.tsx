0746068ad48709ca309df3f4dcea079fbadfb0cf
aman-maharshi <amanm4harshi@gmail.com>
2024-09-02 16:34:53 +0530

    mdx init

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Intro from '@/components/intro'
-import React from 'react'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 const Home = () => {
+  const markdown = `
+    # Hello, world!
+    This is a test of the MDXRemote component.
+  `
+
   return (
     <section className="py-24">
       <div className="container max-w-[1100px]">
         <Intro />
+
+        <MDXRemote source={markdown} />
       </div>
     </section>
   )
